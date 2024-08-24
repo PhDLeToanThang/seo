7d3803e928bd392b90949ede3fab88e760f4a735
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-08-24 17:44:05 +0700

    Update s2_seo_deploy.sh

--- s2_seo_deploy.sh
+++ s2_seo_deploy.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+####################################################
 #Code UNIX:
+# SEO 5.x = Matoto 5.1.1
+# PHP 8.3
+# NGINX 1.24, phpmyadmin 5.2.1
+# mysql 10.11.8-mariaDB-0ubuntu0.22.04.1
+# OS ubuntu 22.04 LTS 
 # D:\Documents\GitHub\seo\seo-deploy.sh
 # Code Deploy SEO server On-premise:
-# Install SEO on Ubuntu 20.04 linux server OS:
-#Matomo is a powerful open source Non-IT Marketing and Behavior management of Customers/visitors (SEO) software tool designed to help you plan trending , tracing or tracking Web buy/shell/e-commerce shop, digital content tracking such as E-book, Reader-paper, device book reader...
-#This is source code deploy for Multi-tenance for more instance SEO.
+# Install SEO on Ubuntu 22.04 linux server OS:
+# Matomo is a powerful open source Non-IT Marketing and Behavior management of Customers/visitors (SEO) software tool 
+# designed to help you plan trending , tracing or tracking Web buy/shell/e-commerce shop, digital content tracking such as E-book, Reader-paper, device book reader...
+# This is source code deploy for Multi-tenance for more instance SEO.
+####################################
 
 #Step 1: Update Ubuntu
 sudo apt update -y
@@ -30,6 +39,8 @@ echo "dbtype name: e.g: mariadb"   # Tên kiểu Database
 read -e dbtype
 echo "dbhost name: e.g: localhost"   # Tên Db host connector
 read -e dbhost
+echo "Your Email address fro Certbot e.g: thang@company.vn" # Địa chỉ email của bạn để quản lý CA
+read -e emailcertbot
 
 GitSEOversion="matomo-latest"
 
@@ -40,24 +51,33 @@ if [ "$run" == n ] ; then
 else
 
 #Step 1. Install NGINX
-sudo apt-get update
-sudo apt-get install nginx
-sudo systemctl stop nginx.service 
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo systemctl stop nginx.service
 sudo systemctl start nginx.service 
 sudo systemctl enable nginx.service
 
 #Step 2. Install MariaDB/MySQL
 #Run the following commands to install MariaDB database for Moode. You may also use MySQL instead.
-sudo apt-get install mariadb-server mariadb-client
+sudo apt-get install mariadb-server mariadb-client -y
 
 #Like NGINX, we will run the following commands to enable MariaDB to autostart during reboot, and also start now.
-sudo systemctl stop mysql.service 
-sudo systemctl start mysql.service 
+sudo systemctl stop mysql.service
+sudo systemctl start mysql.service
 sudo systemctl enable mysql.service
 
 #Run the following command to secure MariaDB installation.
-sudo mysql_secure_installation
+#password mysql mariadb , i'm fixed: M@tKh@uS3cr3t  --> you must changit. 
 
+sudo mysql_secure_installation  <<EOF
+n
+M@tKh@uS3cr3t
+M@tKh@uS3cr3t
+y
+n
+y
+y
+EOF
 #You will see the following prompts asking to allow/disallow different type of logins. Enter Y as shown.
 # Enter current password for root (enter for none): Just press the Enter
 # Set root password? [Y/n]: Y
@@ -70,223 +90,52 @@ sudo mysql_secure_installation
 # After you enter response for these questions, your MariaDB installation will be secured.
 
 #Step 3. Install PHP-FPM & Related modules
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:ondrej/php
+sudo apt-get install software-properties-common -y
+sudo -S add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
-sudo apt install php8.0-fpm php8.0-common php8.0-mbstring php8.0-xmlrpc php8.0-soap php8.0-gd php8.0-xml php8.0-intl php8.0-mysql php8.0-cli php8.0-mcrypt php8.0-ldap php8.0-zip php8.0-curl php8.0-bz2
+sudo apt install php8.3-fpm php8.3-common php8.3-mbstring php8.3-xmlrpc php8.3-soap php8.3-gd php8.3-xml php8.3-intl php8.3-mysql php8.3-cli php8.3-mcrypt php8.3-ldap php8.3-zip php8.3-curl -y
 
 #Open PHP-FPM config file.
-#sudo nano /etc/php/8.0/fpm/php.ini
+
+#sudo nano /etc/php/8.3/fpm/php.ini
 #Add/Update the values as shown. You may change it as per your requirement.
-# if new php.ini configure then clear sign sharp # comment
-cat > /etc/php/8.0/fpm/php.ini <<END
-[PHP]
-engine = On
-short_open_tag = Off
-precision = 14
-output_buffering = 4096
-zlib.output_compression = Off
-implicit_flush = Off
-unserialize_callback_func =
-serialize_precision = -1
-disable_functions = 
-disable_classes =
-zend.enable_gc = On
-zend.exception_ignore_args = On
-zend.exception_string_param_max_len = 0
-expose_php = Off
-file_uploads = On 
-allow_url_fopen = On 
-memory_limit = 1200M 
+cat > /etc/php/8.3/fpm/php.ini <<END
+file_uploads = On
+allow_url_fopen = On
+memory_limit = 1200M
 upload_max_filesize = 4096M
-max_execution_time = 360 
-cgi.fix_pathinfo = 0 
+max_execution_time = 360
+cgi.fix_pathinfo = 0
 date.timezone = asia/ho_chi_minh
 max_input_time = 60
 max_input_nesting_level = 64
 max_input_vars = 5000
 post_max_size = 4096M
-error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT
-display_errors = Off
-display_startup_errors = Off
-log_errors = On
-ignore_repeated_errors = Off
-ignore_repeated_source = Off
-report_memleaks = On
-variables_order = "GPCS"
-request_order = "GP"
-register_argc_argv = Off
-auto_globals_jit = On
-auto_prepend_file =
-auto_append_file =
-default_mimetype = "text/html"
-default_charset = "UTF-8"
-doc_root =
-user_dir =
-enable_dl = Off
-allow_url_fopen = On
-allow_url_include = Off
-default_socket_timeout = 60
-extension=bz2
-extension=curl
-;extension=ffi
-;extension=ftp
-extension=fileinfo
-;extension=gd
-;extension=gettext
-;extension=gmp
-extension=intl
-;extension=imap
-;extension=ldap
-extension=mbstring
-;extension=exif      ; Must be after mbstring as it depends on it
-;extension=mysqli
-;extension=oci8_12c  ; Use with Oracle Database 12c Instant Client
-;extension=oci8_19  ; Use with Oracle Database 19 Instant Client
-;extension=odbc
-extension=openssl
-;extension=pdo_firebird
-;extension=pdo_mysql
-;extension=pdo_oci
-;extension=pdo_odbc
-;extension=pdo_pgsql
-;extension=pdo_sqlite
-;extension=pgsql
-;extension=shmop
-;extension=snmp
-;extension=soap
-;extension=sockets
-;extension=sodium
-;extension=sqlite3
-;extension=tidy
-;extension=xsl
-;zend_extension=opcache
-[CLI Server]
-cli_server.color = On
-[Date]
-;date.timezone =
-; https://php.net/date.default-latitude
-;date.default_latitude = 31.7667
-; https://php.net/date.default-longitude
-;date.default_longitude = 35.2333
-[filter]
-[iconv]
-[imap]
-[intl]
-[sqlite3]
-[Pcre]
-[Pdo]
-[Pdo_mysql]
-pdo_mysql.default_socket=
-[Phar]
-[mail function]
-SMTP = localhost
-; https://php.net/smtp-port
-smtp_port = 25
-;sendmail_from = me@example.com
-;sendmail_path =
-;mail.force_extra_parameters =
-mail.add_x_header = Off
-;mail.log = syslog
-[ODBC]
-odbc.allow_persistent = On
-odbc.check_persistent = On
-odbc.max_persistent = -1
-odbc.max_links = -1
-odbc.defaultlrl = 4096
-odbc.defaultbinmode = 1
-[MySQLi]
-mysqli.max_persistent = -1
-mysqli.allow_persistent = On
-mysqli.max_links = -1
-mysqli.default_port = 3306
-mysqli.default_socket =
-mysqli.default_host =
-mysqli.default_user =
-mysqli.default_pw =
-mysqli.reconnect = Off
-[mysqlnd]
-mysqlnd.collect_statistics = On
-mysqlnd.collect_memory_statistics = Off
-[OCI8]
-[PostgreSQL]
-pgsql.allow_persistent = On
-pgsql.auto_reset_persistent = Off
-pgsql.max_persistent = -1
-pgsql.max_links = -1
-pgsql.ignore_notice = 0
-pgsql.log_notice = 0
-[bcmath]
-bcmath.scale = 0
-[browscap]
-[Session]
-session.save_handler = files
-session.use_strict_mode = 0
-session.use_cookies = 1
-session.use_only_cookies = 1
-session.name = PHPSESSID
-session.auto_start = 0
-session.cookie_lifetime = 0
-session.cookie_path = /
-session.cookie_domain =
-session.cookie_httponly = on
-session.cookie_samesite =
-session.serialize_handler = php
-session.gc_probability = 0
-session.gc_divisor = 1000
-session.gc_maxlifetime = 1440
-session.referer_check =
-session.cache_limiter = nocache
-session.cache_expire = 180
-session.use_trans_sid = 0
-session.sid_length = 26
-session.trans_sid_tags = "a=href,area=href,frame=src,form="
-session.sid_bits_per_character = 5
-[Assertion]
-zend.assertions = -1
-[COM]
-[mbstring]
-[gd]
-[exif]
-[Tidy]
-tidy.clean_output = Off
-[soap]
-soap.wsdl_cache_enabled=1
-soap.wsdl_cache_dir="/tmp"
-soap.wsdl_cache_ttl=86400
-soap.wsdl_cache_limit = 5
-[sysvshm]
-[ldap]
-ldap.max_links = -1
-[dba]
-[opcache]
-[curl]
-[openssl]
-[ffi]
 END
+systemctl restart php8.3-fpm.service
 
-systemctl restart php8.0-fpm.service
+#Step 4. Create Moodle Database
+#Log into MySQL and create database for Moodle.
 
-#Step 4. Create SEO Database
-#Log into MySQL and create database for SEO.
-#!/bin/bash
-mysql -uroot -prootpassword -e "CREATE DATABASE $dbname CHARACTER SET utf8 COLLATE utf8_unicode_ci";
-mysql -uroot -prootpassword -e "CREATE USER '$dbuser'@'$dbhost' IDENTIFIED BY '$dbpass'";
-mysql -uroot -prootpassword -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'$dbhost'";
-mysql -uroot -prootpassword -e "FLUSH PRIVILEGES";
-mysql -uroot -prootpassword -e "SHOW DATABASES";
+# install tool mysql-workbench-community from Tonin Bolzan (tonybolzan)
+sudo snap install mysql-workbench-community
 
-# Nếu đã có thì bỏ qua đoạn hàm này như thế nào ?
+mysql -uroot -prootpassword -e "DROP DATABASE IF EXISTS ${dbname};"
+mysql -uroot -prootpassword -e "CREATE DATABASE IF NOT EXISTS ${dbname} CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql -uroot -prootpassword -e "CREATE USER IF NOT EXISTS '${dbuser}'@'${dbhost}' IDENTIFIED BY \"${dbpass}\";"
+mysql -uroot -prootpassword -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbuser}'@'${dbhost}';"
+mysql -uroot -prootpassword -e "FLUSH PRIVILEGES;"
+mysql -uroot -prootpassword -e "SHOW DATABASES;"
+
 #Step 5. Next, edit the MariaDB default configuration file and define the innodb_file_format:
 #nano /etc/mysql/mariadb.conf.d/50-server.cnf
 #Add the following lines inside the [mysqld] section: 
-# if new php.ini configure then clear sign sharp # comment
-#cat > /etc/mysql/mariadb.conf.d/50-server.cnf <<END
-#[mysqld]
-#innodb_file_format = Barracuda
-#innodb_file_per_table = 1
-#innodb_large_prefix = ON
-#END
+cat > /etc/mysql/mariadb.conf.d/50-server.cnf <<END
+[mysqld]
+innodb_file_format = Barracuda
+innodb_file_per_table = 1
+innodb_large_prefix = ON
+END
 
 #Save the file then restart the MariaDB service to apply the changes.
 systemctl restart mariadb
@@ -403,26 +252,26 @@ sudo ln -s /usr/share/phpmyadmin /var/www/html/$FQDN/$phpmyadmin
 sudo chown -R root:root /var/lib/phpmyadmin
 sudo nginx -t
 
-#Step 11. Nâng cấp PhpmyAdmin lên version 5.2:
+#Step 11. Nâng cấp PhpmyAdmin lên version 5.2.1:
 sudo mv /usr/share/phpmyadmin/ /usr/share/phpmyadmin.bak
 sudo mkdir /usr/share/phpmyadmin/
 cd /usr/share/phpmyadmin/
-sudo wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.tar.gz
-sudo tar xzf phpMyAdmin-5.2.0-all-languages.tar.gz
+sudo wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz
+sudo tar xzf phpMyAdmin-5.2.1-all-languages.tar.gz
 #Once extracted, list folder.
 ls
 #You should see a new folder phpMyAdmin-5.2.0-all-languages
 #We want to move the contents of this folder to /usr/share/phpmyadmin
-sudo mv phpMyAdmin-5.2.0-all-languages/* /usr/share/phpmyadmin
+sudo mv phpMyAdmin-5.2.1-all-languages/* /usr/share/phpmyadmin
 ls /usr/share/phpmyadmin
 mkdir /usr/share/phpMyAdmin/tmp   # tạo thư mục cache cho phpmyadmin 
 
 sudo systemctl restart nginx
-systemctl restart php8.0-fpm.service
+systemctl restart php8.3-fpm.service
 
 #Step 12. Install Certbot
 sudo apt install certbot python3-certbot-nginx -y
-sudo certbot --nginx -d $FQDN
+sudo certbot --nginx -n -d $FQDN --email $emailcertbot --agree-tos --redirect --hsts
 
 # You should test your configuration at:
 # https://www.ssllabs.com/ssltest/analyze.html?d=$FQDN
